b2300d4bedbabf259b438a1c4a5d6033cd814ffc
renatobicego <renatobicego@gmail.com>
2025-10-02 20:14:31 -0300

    metadata

--- mapa-slg/src/app/config.ts
+++ mapa-slg/src/app/config.ts
@@ -1,8 +1,9 @@
 export const siteConfig = {
-  title: "Mapa SLG",
-  description: "Mapa SLG",
-  url: "https://mapa-slg.vercel.app",
-  ogImage: "https://mapa-slg.vercel.app/og.jpg",
+  title: "100 Años - San Luis Gonzaga",
+  description:
+    "Nuestro querido Colegio San Luis Gonzaga cumple 100 años y lo vamos celebrar creando el Mapa del San Lucho. Agregá tu pin y formá parte de esta gran historia",
+  url: "https://centenario.colegiosanluisgonzaga.edu.ar",
+  ogImage: "https://centenario.colegiosanluisgonzaga.edu.ar/og.jpg",
   serverUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
   pages: {
     register: "/registrarse",
--- mapa-slg/src/app/layout.tsx
+++ mapa-slg/src/app/layout.tsx
@@ -21,6 +21,47 @@ const hind = Hind({
 export const metadata: Metadata = {
   title: siteConfig.title,
   description: siteConfig.description,
+  openGraph: {
+    title: siteConfig.title,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: siteConfig.title,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  applicationName: siteConfig.title,
+  authors: [{ name: "Renato Bicego", url: "https://renatobicego.com" }],
+  category: "education",
+  keywords: [
+    "San Luis Gonzaga",
+    "Colegio San Luis Gonzaga",
+    "Mapa SLG",
+    "Mapa San Luis Gonzaga",
+    "Centenario San Luis Gonzaga",
+    "100 años San Luis Gonzaga",
+    "Alumnos San Luis Gonzaga",
+    "Exalumnos San Luis Gonzaga",
+    "Comunidad San Luis Gonzaga",
+    "Educación",
+    "Historia San Luis Gonzaga",
+  ],
 };
 
 export const viewport: Viewport = {
